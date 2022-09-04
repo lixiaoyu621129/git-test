99a9d9bf66f640b2c1aee19dc58a140764c0d17b
909662804@qq.com <621129lxy>
2022-09-04 19:37:50 +0800

    主更新合并

--- src/main/java/com/example/gittest/GitTest.java
+++ src/main/java/com/example/gittest/GitTest.java
@@ -6,8 +6,6 @@ package com.example.gittest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
-        System.out.println("热修复分支~");
-        System.out.println("热修复分支~~");
-        System.out.println("热修复分支~~~~");
+        System.out.println("2022年9月4日19:37:20更新了代码");
     }
 }
