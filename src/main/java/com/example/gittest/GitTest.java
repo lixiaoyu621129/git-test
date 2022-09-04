1333b1619f534f5fbc0f7c76f32137ec125294a1
909662804@qq.com <621129lxy>
2022-09-04 18:22:04 +0800

    first commit

---
+++ src/main/java/com/example/gittest/GitTest.java
@@ -0,0 +1,10 @@
+package com.example.gittest;
+
+/**
+ * @author lixiaoyu
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
