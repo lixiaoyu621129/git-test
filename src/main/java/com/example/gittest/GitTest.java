ce2b9b966255b888f9de093e722045f5aca43586
909662804@qq.com <621129lxy>
2022-09-04 19:12:55 +0800

    Revert "first commit"
    This reverts commit bb6ba062

--- src/main/java/com/example/gittest/GitTest.java
+++ src/main/java/com/example/gittest/GitTest.java
@@ -6,14 +6,5 @@ package com.example.gittest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-        System.out.println("Hello git");
-
-
-
     }
 }
