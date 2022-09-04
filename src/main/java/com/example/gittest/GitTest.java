9e531bbc093e2cba43e7176a9bb2845c93ffd3f9
909662804@qq.com <621129lxy>
2022-09-04 19:25:35 +0800

    热修复提交2

--- src/main/java/com/example/gittest/GitTest.java
+++ src/main/java/com/example/gittest/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("热修复分支~");
+        System.out.println("热修复分支~~");
+        System.out.println("热修复分支~~~~");
     }
 }
