995bd678b43fe7f478a8d427c9617ebc1e0b198c
909662804@qq.com <621129lxy>
2022-09-04 19:26:30 +0800

    master 提交 2

--- src/main/java/com/example/gittest/GitTest.java
+++ src/main/java/com/example/gittest/GitTest.java
@@ -7,7 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("主版本");
-
-
+        System.out.println("主版本02");
     }
 }
