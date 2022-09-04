90a8ad0a96cd1dea7e3620a8fc8d8ddb29242cd6
909662804@qq.com <621129lxy>
2022-09-04 19:23:13 +0800

    热修复分支1

--- src/main/java/com/example/gittest/GitTest.java
+++ src/main/java/com/example/gittest/GitTest.java
@@ -5,23 +5,7 @@ package com.example.gittest;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
-
-
+        System.out.println("Hello git");
+        System.out.println("热修复分支~");
     }
 }
